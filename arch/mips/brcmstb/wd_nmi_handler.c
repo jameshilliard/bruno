9a05d8c6c96e158e54ed6e0977cf1e0b473ede43
Daniel Mentz <danielmentz@google.com>
2013-03-21 19:36:06 -0700

    Route watchdog triggered NMI to both CPU threads
    The default setting of the Broadcom Mode Register (CP0 Register 22,
    Select 1) is to route NMIs only to Thread 0. As a result, we only get
    stacktraces from Thread 0.
    
    With this change, the NMI handler prints a stacktrace before it changes
    the NMI routing to route the already asserted NMI to Thread 1 as well.
    This will immediately cause an NMI exception on Thread 1, and we get a
    stacktrace from there, too.
    
    Change-Id: Idc10ff5b17146c8c1edbc9e5548323627440e36a

--- arch/mips/brcmstb/wd_nmi_handler.c
+++ arch/mips/brcmstb/wd_nmi_handler.c
@@ -3,7 +3,9 @@
  * hardware watchdog
  *
  * - print registers
- * - panic
+ * - set mode register to also route this NMI to Thread 1
+ * - wait for a short while
+ * - restart
  */
 #include <linux/init.h>
 #include <linux/reboot.h>
@@ -20,6 +22,15 @@ static NORET_TYPE void ATTRIB_NORET bruno_nmi_exception_handler(struct pt_regs *
 	console_verbose();
 	printk(KERN_EMERG "NMI taken on CPU %d. Probably hardware watchdog timeout.\n",cpu);
 	show_registers(regs);
+	/* Broadcom Mode Register (CP0 Register 22, Select 1)
+	 * 0x1: NMI is routed to Thread 0.
+	 * 0x2: NMI is routed to Thread 1.
+	 * 0x3: NMI is routed to both threads.
+	 * Change NMI exception routing to route NMI to both threads. Since the
+	 * NMI is already asserted, this will cause an NMI exception on Thread
+	 * 1 immediately.
+	 * */
+	write_c0_brcm_mode(read_c0_brcm_mode() | 0x3);
 	printk(KERN_EMERG "Rebooting in %d seconds..", timeout);
 	while (1) {
 		mdelay(timeout*1000);
